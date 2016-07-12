b756df34c6340fb7d1a6a61bc169740eea90250c
alexbadaloff <alex.badalove@gmail.com>
2016-07-12 23:37:25 +0300

    Fix get correct value from mh-z19.

--- esp8266_mh_z19_test.ino
+++ esp8266_mh_z19_test.ino
@@ -1,38 +1,46 @@
-#define pwmPin 5
+#define pwmPin 12 // gpio12
 
 int prevVal = LOW;
 long th, tl, h, l, ppm, ppm1;
+long tt;
+int myVal;
 
 void setup() {
   Serial.begin(115200);
   pinMode(pwmPin, INPUT);
-  ppm1 = 0;
+}
+
+int getPPM() {
+  int j = 0, ppm;
+  long t1 = millis();
+  while (1) { // начинаем опрос mh-z19
+    tt = millis();
+    myVal = digitalRead(pwmPin);
+    if (myVal == HIGH) {
+      if (myVal != prevVal) {
+        h = tt;
+        tl = h - l;
+        prevVal = myVal;
+      }
+    }  else {
+      if (myVal != prevVal) {
+        l = tt;
+        th = l - h;
+        prevVal = myVal;
+        ppm = 5000 * (th - 2) / (th + tl - 4);
+        if (j > 0) { // первое значение пропускаем, в нём мусор
+          return ppm;
+        }
+        j++;
+      }
+    }
+    if ((millis() - t1) > 3000) {
+      return -1;
+    }
+  }
 }
 
 void loop() {
-  long tt = millis();
-  int myVal = digitalRead(pwmPin);
-  
-  // Если обнаружили изменение 
-  if (myVal == HIGH) {
-    if (myVal != prevVal) {
-      h = tt;
-      tl = h - l;
-      prevVal = myVal;
-    }
-  } 
-  else
-  {
-    if (myVal != prevVal) {
-      l = tt;
-      th = l - h;
-      prevVal = myVal;
-      ppm = 5000 * (th - 2) / (th + tl - 4);
-      if ( ppm != ppm1 ) // чтобы не выводить повторяющиеся значения
-      {
-        ppm1 = ppm;
-        Serial.println("PPM = " + String(ppm));
-      }
-    }
-  }
+  Serial.println(String(getPPM()) + "   " + String(millis()));
+  delay(20000);
 }
