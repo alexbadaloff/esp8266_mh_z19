eef396380f17fddb681c818e659fee401c7c9269
alexbadaloff <alex.badalove@gmail.com>
2016-10-14 21:56:23 +0300

    Сompletely rethinking. All code rewritten.

--- esp8266_mh_z19_test.ino
+++ esp8266_mh_z19_test.ino
@@ -1,44 +1,43 @@
 #define pwmPin 12 // gpio12
 
-int prevVal = LOW, myVal;
-long th, tl, h, l, tt;
-
 void setup() {
   Serial.begin(115200);
   pinMode(pwmPin, INPUT);
 }
 
-int getPPM() {
-  int j = 0, ppm;
-  long t1 = millis();
+int myPPM() {
+  int  ppm, val;
+  int step = 0;
+  long th, tl;
+
   while (1) { // начинаем опрос mh-z19
-    tt = millis();
-    myVal = digitalRead(pwmPin);
-    if (myVal == HIGH) {
-      if (myVal != prevVal) {
-        h = tt;
-        tl = h - l;
-        prevVal = myVal;
+    delay(2);
+    val = digitalRead(pwmPin);
+    if (val == HIGH) {
+      if (step > 0) {
+        th = millis();
+        step = 2;
+      } else {
+        continue;
       }
-    }  else {
-      if (myVal != prevVal) {
-        l = tt;
-        th = l - h;
-        prevVal = myVal;
+    } else {
+      if (step < 2) {
+        tl = millis();
+        if (step < 1)
+          step = 1;
+      }
+      else
+      {
+        th = th - tl;
+        tl = 1004 - th;
         ppm = 5000 * (th - 2) / (th + tl - 4);
-        if (j > 0) { // первое значение пропускаем, в нём мусор
-          return ppm;
-        }
-        j++;
+        return ppm;
       }
-    }
-    if ((millis() - t1) > 3000) { // на случай, если не получим значение в течении 3 секунд
-      return -1;
     }
   }
 }
 
 void loop() {
-  Serial.println(String(getPPM()) + "   " + String(millis()));
-  delay(20000);
+  Serial.println("CO2: " + String(myPPM()));
+  delay(10000);
 }
